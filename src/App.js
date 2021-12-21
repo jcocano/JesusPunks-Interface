7c750eb036fea61753e3f6fbe141326a8be735b4
Jesus Cocaño <jesus.cocano@gmail.com>
2021-12-21 11:25:20 -0600

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './views/home';
+import Web3 from 'web3';
+
 
 function App() {
+
+  useEffect(() =>{
+    if (window.ethereum){
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route path="/" exact component={Home} />
   );
 }
 
